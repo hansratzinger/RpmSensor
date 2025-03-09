dc0cad343b0d82ee5c020f03e16b7861bc5c0ab2
hansratzinger <hansratzinger>
2025-03-09 21:44:35 +0100

    1.1.0  Paket ID for RPM added HR 2025-02-08

--- src/RpmSensor.cpp
+++ src/RpmSensor.cpp
@@ -3,11 +3,15 @@
 // and outputs the RPM value every second
 // based on FDRS library
 // FDRS Node sends the RPM value to the FDRS gateway
-// FARM DATA RELAY SYSTEM
+// uses libray of FARM DATA RELAY SYSTEM
 // Developed by Timm Bogner (timmbogner@gmail.com) in Urbana, Illinois, USA.
 //
 // HR 2025-02-06  1.0.0  Initial version
 // HR 2025-02-08  1.1.0  Paket ID for RPM added
+
+// Status LED 
+#define GREEN_LED  25    // Grüne LED an GPIO25 (output-fähig)
+#define RED_LED    26    // Rote LED an GPIO26 (output-fähig)
 
 
 #include <Arduino.h>
@@ -19,6 +23,11 @@
 #define RPM              20 // RPM (Iterations genannt in FDRS)
 #define RPM_PAKETSEND_ID 16 // RPM (Iterations genannt in FDRS)
 
+#define TRIGGERS_PER_REV 1 // Anzahl der Impulse pro Umdrehung
+
+// Status LED 
+#define GREEN_LED  25    // Grüne LED an GPIO25 (output-fähig)
+#define RED_LED    26    // Rote LED an GPIO26 (output-fähig)
 
 volatile unsigned long Rpm_Count; // Zähler für Interrupts
 float Rpm;                // Variable für die berechnete Drehzahl
@@ -36,10 +45,16 @@ void IRAM_ATTR Rpm_isr() {
   if (interruptTime - lastInterruptTime > 1) { // Entprellzeit von 1ms
     Rpm_Count++;
     lastInterruptTime = interruptTime;
-    digitalWrite(LED_PIN, HIGH); // LED einschalten
+    digitalWrite(GREEN_LED, HIGH); // LED einschalten
     delay(1);                     // Kurze Verzögerung, um das Aufleuchten sichtbar zu machen
-    digitalWrite(LED_PIN, LOW);  // LED ausschalten
+    digitalWrite(GREEN_LED, LOW);  // LED ausschalten
   }
+}
+
+// LED Funktion vereinfacht
+void setLed(bool state, uint8_t pin) {
+  pinMode(pin, OUTPUT);
+  digitalWrite(pin, state);
 }
 
 void sendFDRS(float data1, float data2) {   // Sendet die RPM-Werte an den FDRS-Gateway 
@@ -48,16 +63,18 @@ void sendFDRS(float data1, float data2) {   // Sendet die RPM-Werte an den FDRS-
   // DBG(sendFDRS()); // Debugging 
   if (sendFDRS()) {
     DBG("Big Success!");
+    setLed(false, RED_LED);
   } else {
     DBG("Nope, not so much.");
+    setLed(true, RED_LED);
   }
 }
 
-
 void setup() {
+  setLed(true, RED_LED);
   Serial.begin(115200);
   pinMode(IR_SENSOR_PIN, INPUT_PULLUP); // Setze den IR-Sensor-Pin als Eingang mit Pull-Up Widerstand
-  pinMode(LED_PIN, OUTPUT);          // Setze den LED-Pin als Ausgang
+  pinMode(LED_PIN, OUTPUT);          
 
   beginFDRS(); // Initialisiere FDRS
 
@@ -70,6 +87,7 @@ void setup() {
   lastOutputTime = millis();
 
   attachInterrupt(digitalPinToInterrupt(IR_SENSOR_PIN), Rpm_isr, FALLING); // Interrupt bei fallender Flanke
+  setLed(false, RED_LED);
 }
 
 void loop() {
@@ -81,7 +99,7 @@ void loop() {
     unsigned long impulseCount = Rpm_Count - Rpm_Count_LastSecond;
 
     // Berechne die RPM basierend auf der Anzahl der Impulse pro Sekunde
-    Rpm = ((float)impulseCount * 60) / measurementTime / 3;
+    Rpm = ((float)impulseCount * 60) / measurementTime / TRIGGERS_PER_REV;
 
     Serial.print(">RPM:"); // Formatierung für die Ausgabe auf Teleplot (optional)
     Serial.println(Rpm);
