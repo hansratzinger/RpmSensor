f0c7b3718be9a07d65bfe0adc917363918e4270c
hansratzinger <hansratzinger>
2025-04-12 12:59:23 +0200

    RELEASE 1.0 HR 2025-04-12 13:00 NK

--- src/RpmSensorRtcOled.cpp
+++ src/RpmSensorRtcOled.cpp
@@ -1,14 +1,21 @@
-// Project: RpmSensorRtcOled
-// HR 2025-04-10 00:45 NK
-
-/*********
-  Rui Santos & Sara Santos - Random Nerd Tutorials
-  Complete instructions at https://RandomNerdTutorials.com/esp32-ds3231-real-time-clock-arduino/
-*********/
-
-// based on the RTClib: implementation of an alarm using DS3231 https://github.com/adafruit/RTClib
-
-// OLED display library
+// -------------------------------------------------
+// RpmSensorRtcOled.cpp
+// -------------------------------------------------
+// Usage for measuring RMP (Revolutions per Minute) with an infrared sensor
+// and displaying the result on an OLED display. The DS3231 RTC is used to keep track of time.
+// The SD card is used to log the data. The program allows setting the RTC time using buttons on the ESP32 board.
+// The program is designed to run on an ESP32 board with a DS3231 RTC, an OLED display, and an SD card module.
+// The program uses the TFT_eSPI library for the OLED display and the RTClib library for the DS3231 RTC.
+// The program also uses the SPI library for communication with the SD card module.
+// The program is designed to be used with the ESP32 board and is not compatible with other boards.
+// ESP32 DS3231 RTC + OLED Display + SD Card + Infrared Sensor
+// RELEASE 1.0 HR 2025-04-12 NK
+// -------------------------------------------------
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// any later version.
+// --------------------------------------------------
 #include <Arduino.h>
 #include "TFT_eSPI.h"
 #include "User_Setup.h" // TFT_eSPI library setup file
